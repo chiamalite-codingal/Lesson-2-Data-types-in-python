8fdec72016451b4db830ef7cd47e71ac8eb04192
chiamalite-codingal <chiamalitecodingal@gmail.com>
2024-09-06 17:30:40 +0100

    Datatypes

---
+++ main.py
@@ -0,0 +1,18 @@
+# datatypes
+# 1.string datatype
+x ="chiamalite" #string\
+age = "2+8" #string
+print(age)
+print(x)
+
+#2.integer datatype(numbers without points)
+age = 28
+print(age)
+
+#3. float datatype (numbers with points)
+weight = 50.5
+print(weight)
+
+#4. boolean datatype
+student = "chiamalite"
+print(False)
